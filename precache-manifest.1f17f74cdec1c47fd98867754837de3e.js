15035fe414681bd9c96c64a9bbb845536f5ef5a0
Ccino Lee <ccinodev@gmail.com>
2020-06-13 16:49:39 +0900

    Updates

---
+++ precache-manifest.1f17f74cdec1c47fd98867754837de3e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b7bccb340ff4867ff0a",
+    "url": "/fokin-weather/static/js/app.ac7a5131.chunk.js"
+  },
+  {
+    "revision": "73ac5eacc3e321c1998b",
+    "url": "/fokin-weather/static/js/runtime~app.96a99b0c.js"
+  },
+  {
+    "revision": "9701ef2ac0b58050dd2b",
+    "url": "/fokin-weather/static/js/2.336da2a3.chunk.js"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/fokin-weather/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/fokin-weather/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/fokin-weather/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/fokin-weather/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fokin-weather/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/fokin-weather/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/fokin-weather/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/fokin-weather/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/fokin-weather/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/fokin-weather/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/fokin-weather/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/fokin-weather/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/fokin-weather/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/fokin-weather/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/fokin-weather/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/fokin-weather/favicon.ico"
+  },
+  {
+    "revision": "c3cfb08b6713a3cee6b399f76580d1cd",
+    "url": "/fokin-weather/index.html"
+  },
+  {
+    "revision": "5fe708ccca82e21dc5eb2adc28828bc3",
+    "url": "/fokin-weather/manifest.json"
+  },
+  {
+    "revision": "0047cf4a0df7815c3253729dad9cc297",
+    "url": "/fokin-weather/register-service-worker.js"
+  }
+];
